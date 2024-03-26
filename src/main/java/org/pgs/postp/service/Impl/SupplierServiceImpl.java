91eaa332f3fb69294b3ea2490813344a28157c8e
devyanijchauhan <devyanichauhan3947@gmail.com>
2024-03-26 12:07:28 +0530

    SupplierServiceImpl change

--- src/main/java/org/pgs/postp/service/Impl/SupplierServiceImpl.java
+++ src/main/java/org/pgs/postp/service/Impl/SupplierServiceImpl.java
@@ -118,10 +118,14 @@ public class SupplierServiceImpl implements SupplierService {
 
     @Override
     public void processCSV(MultipartFile file) throws IOException {
+        // Check CSV file headers
         BufferedReader br = new BufferedReader(new InputStreamReader(file.getInputStream()));
-
-        // Skip the header line
-        br.readLine();
+        String headerLine = br.readLine();
+        if (headerLine == null || !headerLine.equals("SupplierAgency, ContactPerson, SupplierEmail, SupplierPhone, ContactPersonEmail, ContactPersonPhone, Address")) {
+            throw new IllegalArgumentException("Invalid CSV file format or headers");
+        }
+//        // Skip the header line
+//        br.readLine();
 
         String line;
 
