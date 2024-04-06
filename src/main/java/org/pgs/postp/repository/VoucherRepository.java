4a3778962d797f93c75f0927f83ba59e3f831fd5
devyanijchauhan <devyanichauhan3947@gmail.com>
2024-04-06 21:23:04 +0530

    voucherrepository change

--- src/main/java/org/pgs/postp/repository/VoucherRepository.java
+++ src/main/java/org/pgs/postp/repository/VoucherRepository.java
@@ -7,4 +7,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface VoucherRepository extends JpaRepository<VoucherModel, Long> {
 
+    VoucherModel findByVoucherCode(String voucherCode);
+
+
 }
