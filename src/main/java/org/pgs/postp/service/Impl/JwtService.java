abb33fd5ee42d07be95946ee7d9beb9c52861474
devyanijchauhan <devyanichauhan3947@gmail.com>
2024-04-16 11:55:58 +0530

    JWT service

--- src/main/java/org/pgs/postp/service/Impl/JwtService.java
+++ src/main/java/org/pgs/postp/service/Impl/JwtService.java
@@ -59,7 +59,7 @@ public class JwtService {
                 .setClaims(claims)
                 .setSubject(username)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .setExpiration(new Date(System.currentTimeMillis() + 24 * 60 * 60 * 1000))
                 .signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
     }
 
